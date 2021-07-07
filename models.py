2baf69bfeb66fe83aea1b8383311f4add076bf8e
gerardponsds <86711777+gerardponsds@users.noreply.github.com>
2021-07-07 16:59:25 +0200

    Added the two models.

--- models.py
+++ models.py
@@ -9,13 +9,27 @@ def final_size(input_size,num_conv,pad=2):
   return input_size
 
 
-
-def model(num_conv = 3,input_size = 250):
+# MODEL TO DIFERENTIATE METEORITES AND ROCKS FROM OTHER OBJECTES
+def model_rocks(num_conv = 3,input_size = 250):
 
 	size = final_size(input_size,num_conv)
 
-	m = nn.Sequential(nn.Conv2d(3,64,kernel_size=3),nn.MaxPool2d(kernel_size=2),nn.ReLU(),
-						  nn.Conv2d(64,64,kernel_size=3),nn.MaxPool2d(kernel_size=2),nn.ReLU(),
+	m = nn.Sequential(nn.Conv2d(3,32,kernel_size=3),nn.MaxPool2d(kernel_size=2),nn.ReLU(),
+						  nn.Conv2d(32,64,kernel_size=3),nn.MaxPool2d(kernel_size=2),nn.ReLU(),
+						  nn.Conv2d(64,128,kernel_size=3),nn.MaxPool2d(kernel_size=2),nn.ReLU(),
+						  nn.Flatten(),nn.Dropout(0.1),nn.Linear(128*size*size,128),nn.ReLU(),
+						  nn.Linear(128,1),nn.Sigmoid()
+						  )
+	return m
+
+# MODEL TO DIFERENTIATE METEORITES FROM ROCKS
+
+def model_meteorites(num_conv = 3,input_size = 250):
+
+	size = final_size(input_size,num_conv)
+
+	m = nn.Sequential(nn.Conv2d(3,32,kernel_size=3),nn.MaxPool2d(kernel_size=2),nn.ReLU(),
+						  nn.Conv2d(32,64,kernel_size=3),nn.MaxPool2d(kernel_size=2),nn.ReLU(),
 						  nn.Conv2d(64,128,kernel_size=3),nn.MaxPool2d(kernel_size=2),nn.ReLU(),
 						  nn.Flatten(),nn.Dropout(0.1),nn.Linear(128*size*size,160),nn.ReLU(),
 						  nn.Linear(160,1),nn.Sigmoid()
